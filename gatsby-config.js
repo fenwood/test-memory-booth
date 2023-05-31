ab570f1cb88634125136d06f53b187e47c6f6b8a
fenwood <fenwood@gmail.com>
2023-05-31 09:19:49 -0400

    Uploaded remainder

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+module.exports = {
+  siteMetadata: {
+    title: "Memory Booths",
+    description: "Photobooth rentals in Belleville, Kingston, and the GTA",
+    author: "Memory Booth",
+    twitterUsername: "#",
+    facebookUsername: "#",
+    instagramUsername: "#",
+    linkedinUsername: "#",
+    image: "assets/bg-camera-hand.png",
+    siteUrl: "https://barcadia.netlify.com",
+    developerName: "Jeremy Thompson",
+    developerUrl: "https://www.jeremythompson.ca",
+  },
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "static/favicon.ico",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://barcadia.netlify.com",
+        sitemap: "https://barcadia.netlify.com/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+  ],
+}
